4de538e103b878325fa373c5a4c4d01fdc5a80bc
Michal Šára <2155397+MikeshCZ@users.noreply.github.com>
2024-06-10 22:35:06 +0200

    implementation of raygui. Change help to menu using raygui window. Format of the code and comments. Solution for release set up to opt WinMain.

--- Game of Life/main.cpp
+++ Game of Life/main.cpp
@@ -1,11 +1,14 @@
-﻿#include <INIReader.h>
-#include <raylib.h>
-#include <iostream>
+﻿#include <iostream>
 #include <fstream>
 #include <string>
 #include <algorithm>
+#include "raylib.h"
+#include "INIReader.h"
 #include "simulation.h"
 #include "config.h"
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
 
 int main()
 {
@@ -13,13 +16,10 @@ int main()
 	// # READ CONFIG FILE #
 	// ####################
 
-	Config config;
-	int initOK = config.Initialization();
-
-	// If reading of the config fail return 1
-	if (initOK == 1) { return 1; }
-	
-	INIReader reader(config.GetFileName());
+	Config config;								// Read the configuration file
+	int initOK = config.Initialization();		// inih configuration inicialization (return 0 => OK)
+	if (initOK == 1) { return 1; }				// If reading of the config fail return 1
+	INIReader reader(config.GetFileName());		// Reader inih to read from config file
 	
 	// ##################
 	// # INICIALIZATION #
@@ -29,57 +29,52 @@ int main()
 	// Constants
 	// ---------
 
-	const int WINDOW_WIDTH = reader.GetInteger("window", "width", 1280);
-	const int WINDOW_HEIGHT = reader.GetInteger("window", "height", 720);
-	const int GRID_WIDTH = WINDOW_WIDTH;
-	const int GRID_HEIGHT = WINDOW_HEIGHT - 30;
-	const int CELL_SIZE = reader.GetInteger("cell", "size", 5);
-	const Color EDGE = { reader.GetInteger("color","edgeColorR",60), reader.GetInteger("color","edgeColorG",60), reader.GetInteger("color","edgeColorB",60), 255 };
-	const std::string GAME_NAME = "Mikesh's Game of Life";
-	const std::string HYPEN = " | ";
-	const char* windowTitle = GAME_NAME.c_str();
-	
-	// Text for the F1 Help window
-	const char* helpText = 
-		"F1							Show/Close this help\n\n"
-		"Left Click			Add cell state (alive).\n"
-		"Right Click			Remove cell state (dead).\n\n"
-		"Space					Start/stop the simulation.\n"
-		"ENTER				Clear / Reset the board.\n\n"
-		"Key up				Increase the simulation speed.\n"
-		"Key down			Decrease the simulation speed.\n"
-		"Key left			Increase cell edges.\n"
-		"Key right			Decrease cell edges.\n\n"
-		"F								Toggle fullscreen ON/OFF.\n"
-		"C								Turn on/off random colors.\n\n"
-		"F10						Exit the game.";
+	const int WINDOW_WIDTH = reader.GetInteger("window", "width", 1280);	// Main window width
+	const int WINDOW_HEIGHT = reader.GetInteger("window", "height", 720);	// Main window height
+	const int GRID_WIDTH = WINDOW_WIDTH;									// Grid width
+	const int GRID_HEIGHT = WINDOW_HEIGHT - 30;								// Grid height
+	const int CELL_SIZE = reader.GetInteger("cell", "size", 5);				// One cell size
+	const Color EDGE = {													// Read edge color from the config
+		reader.GetInteger("color","edgeColorR",60), 
+		reader.GetInteger("color","edgeColorG",60), 
+		reader.GetInteger("color","edgeColorB",60), 
+		255 };
+	const std::string GAME_NAME = "Mikesh's Game of Life";					// Game name in the title
+	const std::string HYPEN = " | ";										// Hypen for the text in the top bar
+	const char* windowTitle = GAME_NAME.c_str();							// Window title
+	const char* helpText =													// Text for the F1 Help window
+		"[F1] - Show/Close this help\n\n"
+		"[Left Click] - Add cell state (alive).\n"
+		"[Right Click] - Remove cell state (dead).\n\n"
+		"[Space] - Start/stop the simulation.\n"
+		"[ENTER] - Clear / Reset the board.\n\n"
+		"[Key up] - Increase the simulation speed.\n"
+		"[Key down] - Decrease the simulation speed.\n"
+		"[Key left] - Increase cell edges.\n"
+		"[Key right] - Decrease cell edges.\n\n"
+		"[F] - Toggle fullscreen ON/OFF.\n"
+		"[C] - Turn on/off random colors.\n\n"
+		"[F10] - Exit the game.";
 	
 	// -------------
 	// Set variables
 	// -------------
 	
-	// Target FPS of the app
-	int fps = reader.GetInteger("window", "fps", 30);
+	
+	int fps = reader.GetInteger("window", "fps", 30);					// Target FPS of the app
 	fps = std::clamp(fps, 1, 240);
-
-	// Width of the cell edges
-	int edgeWidth = reader.GetInteger("cell", "edgeWidth", 1);
+	
+	int edgeWidth = reader.GetInteger("cell", "edgeWidth", 1);			// Width of the cell edges
 	edgeWidth = std::clamp(edgeWidth, 0, CELL_SIZE - 1);
 	
-	// Random colors feature 
-	bool randomColors = reader.GetBoolean("cell","randomColors", true);
-
-	// Fullscreen
-	bool fullscreen = reader.GetBoolean("window", "fullscreen", false);
-	
-	// State of the cell (live/dead)
-	bool state;
-	
-	// Show help window
-	bool showHelp = false;
-
-	// Main Help widnow as rectangle
-	Rectangle helpWindow = { WINDOW_WIDTH/2 - 400/2, WINDOW_HEIGHT/2 - 280/2, 400, 280 };
+	bool randomColors = reader.GetBoolean("cell","randomColors", true);	// Random colors feature 
+	bool fullscreen = reader.GetBoolean("window", "fullscreen", false);	// Fullscreen
+	bool state;															// State of the cell (live/dead)
+	bool IsMenuActive = false;											// Show help window
+	Vector2 mousePosition{};											// Reading actual position of the mouse in the window 
+	Vector4 menuWindow = { 500, 200, 300, 400 };						// Menu window parameter x, y, width, height
+	Vector2 panOffset{};												// Ofset of the mouse when dragging the main window
+	bool dragMenu = false;												// Dragging the menu window
 
 	// ----------------
 	// Init Main window
@@ -94,15 +89,21 @@ int main()
 	// Create and set up simulation
 	// ----------------------------
 
-	// Main simulation class
-	Simulation simulation(GRID_WIDTH, GRID_HEIGHT, CELL_SIZE);
-	
-	// Set up edge width
-	simulation.SetEdges(edgeWidth);
+	Simulation simulation(GRID_WIDTH, GRID_HEIGHT, CELL_SIZE);			// Main simulation class
+	simulation.SetEdges(edgeWidth);										// Set up edge width
 	
 	// Set up colors from INI file
-	simulation.SetColor(true, reader.GetInteger("color", "liveColorR", 0), reader.GetInteger("color", "liveColorG", 170), reader.GetInteger("color", "liveColorB", 170), reader.GetInteger("color", "liveColorA", 255));
-	simulation.SetColor(false, reader.GetInteger("color", "deadColorR", 140), reader.GetInteger("color", "deadColorG", 100), reader.GetInteger("color", "deadColorB", 80), reader.GetInteger("color", "deadColorA", 130));
+	simulation.SetColor(true, 
+		reader.GetInteger("color", "liveColorR", 0), 
+		reader.GetInteger("color", "liveColorG", 170), 
+		reader.GetInteger("color", "liveColorB", 170), 
+		reader.GetInteger("color", "liveColorA", 255));
+
+	simulation.SetColor(false, 
+		reader.GetInteger("color", "deadColorR", 140), 
+		reader.GetInteger("color", "deadColorG", 100), 
+		reader.GetInteger("color", "deadColorB", 80), 
+		reader.GetInteger("color", "deadColorA", 130));
 	
 	// randomColors from INI file
 	if (randomColors != simulation.IsRandomColors()) { simulation.ToggleRandomColors(); }
@@ -117,9 +118,11 @@ int main()
 		// -----------------
 		// 1. Event handling
 		// -----------------
+		
+		mousePosition = GetMousePosition();
 
-		//  Mouse click
-		if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT))
+		//  Mouse click when menu is not active
+		if ((IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) && !IsMenuActive)
 		{
 			if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
 			{
@@ -130,11 +133,28 @@ int main()
 				state = false;
 			}
 			
-			Vector2 mousePosition = GetMousePosition();
-			int row = (mousePosition.y - 30) / CELL_SIZE;
+			int row = (mousePosition.y - 30) / CELL_SIZE;	// minus top bar width 30 px
 			int column = mousePosition.x / CELL_SIZE;
 			simulation.ToggleCell(row, column, state);
 		}
+		// Left mouse click when menu is open
+		else if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) && !dragMenu && IsMenuActive)
+		{
+			if (CheckCollisionPointRec(mousePosition, { menuWindow.x, menuWindow.y, menuWindow.z-20, 20 }))
+			{
+				dragMenu = true;
+				panOffset = { (mousePosition.x - menuWindow.x), (mousePosition.y - menuWindow.y) };
+			}
+		}
+		// drag menu window
+		if (dragMenu)
+		{
+			menuWindow.x = (mousePosition.x - panOffset.x);
+			menuWindow.y = (mousePosition.y - panOffset.y);
+
+			if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) dragMenu = false;
+		}
+
 		// Start/Stop with SPACE KEY
 		if (IsKeyPressed(KEY_SPACE))
 		{
@@ -213,9 +233,9 @@ int main()
 		{
 			ToggleFullscreen();
 		}
-		else if (IsKeyPressed(KEY_F1) || (showHelp && IsKeyPressed(KEY_ESCAPE)))
+		else if (IsKeyPressed(KEY_F1) || (IsMenuActive && IsKeyPressed(KEY_ESCAPE)))
 		{
-			showHelp = !showHelp;
+			IsMenuActive = !IsMenuActive;
 		}
 		 
 		// -----------------
@@ -232,32 +252,28 @@ int main()
 		// 3. Drawing
 		// ----------
 		
-		// Update windows title
+		// Update top bar title
 		std::string runStatus = simulation.IsRunning() ? "Running" : "Pause";
 		std::string fpsStatus = "Target FPS: " + std::to_string(fps);
-		std::string strInfo = GAME_NAME + HYPEN + runStatus + HYPEN + fpsStatus + HYPEN + "F1 HELP" + HYPEN + "Generation: " + std::to_string(simulation.GetGeneration());
+
+		std::string strInfo = GAME_NAME + HYPEN + runStatus + HYPEN + fpsStatus + HYPEN + "Press F1 to open MENU" + HYPEN + "Generation: " + std::to_string(simulation.GetGeneration());
 		const char* info = strInfo.c_str();
 
 		// Drawing the window
 		BeginDrawing();
-			ClearBackground(EDGE);
-			DrawRectangle(0, 0, WINDOW_WIDTH, 30, BLACK);
-			DrawText(info, 5, 5, 20, WHITE);
-			DrawFPS(WINDOW_WIDTH - 90, 5);
-			simulation.Draw();
-			if (showHelp)
+			ClearBackground(EDGE);							// Clear the background
+			DrawRectangle(0, 0, WINDOW_WIDTH, 30, BLACK);	// Draw top bar
+			DrawText(info, 5, 5, 20, WHITE);				// Write top bar text
+			DrawFPS(WINDOW_WIDTH - 90, 5);					// Write FPS to right corner
+
+			simulation.Draw();								// Draw new grid
+			
+			if (IsMenuActive)								// F1 Menu open
 			{
-				DrawRectangleRec(helpWindow, Fade(SKYBLUE, 0.5f));
-				DrawRectangleLinesEx(helpWindow, 3, BLUE);
-				DrawText(helpText, helpWindow.x + 10, helpWindow.y + 10, 16, DARKGRAY);
+				IsMenuActive = !GuiWindowBox({menuWindow.x, menuWindow.y, menuWindow.z, menuWindow.w}, "#198#Menu");
+				DrawText(helpText, menuWindow.x + 10, menuWindow.y + 30, 10, DARKGRAY);			
 			}
 		EndDrawing();
 	}
 	CloseWindow();
-}
-
-// Definition of WinMain for Windows subsystem
-int WinMain()
-{
-	return main();
 }
